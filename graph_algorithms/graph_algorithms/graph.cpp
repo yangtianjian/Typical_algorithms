bf98be5cef8ba4a6c50be2da27b46607c11620c8
yangtianjian <1076338654@qq.com>
2019-09-19 22:26:27 +0800

    Complete the dense graph class

---
+++ graph_algorithms/graph_algorithms/graph.cpp
@@ -0,0 +1,9 @@
+//
+//  graph.cpp
+//  graph_algorithms
+//
+//  Created by apple on 2019/9/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#include "graph.hpp"
---
+++ graph_algorithms/graph_algorithms/graph.hpp
@@ -0,0 +1,14 @@
+//
+//  graph.hpp
+//  graph_algorithms
+//
+//  Created by apple on 2019/9/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#ifndef graph_hpp
+#define graph_hpp
+
+#include <stdio.h>
+
+#endif /* graph_hpp */
